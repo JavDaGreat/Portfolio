1904ba01c2f7489d78bc6d2604f61c42b65aa209
JavDaGreat <98164907+JavDaGreat@users.noreply.github.com>
2023-07-20 18:46:18 +0200

    1.0

---
+++ src/components/data/data.js
@@ -0,0 +1,28 @@
+import css from "../../assets/css.png"
+import html from "../../assets/html.png"
+
+export const data=[
+    {
+        id:1,
+        name:"React JS Application",
+        image:css,
+        github:"",
+        live:"",
+    },
+    {
+        id:2,
+        name:"React JS Application",
+        image:html,
+        github:"",
+        live:"",
+    },
+    {
+        id:3,
+        name:"React JS Application",
+        image:css,
+        github:"",
+        live:"",
+    },
+ 
+
+]
