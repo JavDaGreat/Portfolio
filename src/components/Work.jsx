e8fead9d0e792192b6b15fc344f1c7c9b46d935b
JavDaGreat <98164907+JavDaGreat@users.noreply.github.com>
2023-07-28 15:01:35 +0200

    update

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -7,7 +7,7 @@ const Work = () => {
     const project = data;
   
   return (
-    <div name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
+    <div name='work' className='w-full h-screen text-gray-300 bg-[#0a192f]'>
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
         <div className='pb-8'>
           <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>
@@ -16,14 +16,14 @@ const Work = () => {
           <p className='py-6'>// Check out some of my recent work</p>
         </div>
 
-<div className="grid sm:grid-cols-2 md:grid-cols-3 h-[300px] ">
+<div className=" w-full h-[350px] gap-8 grid grid-cols-3 ">
           
           {project.map((item, index) => (
   <div
     key={index}
     style={{ backgroundImage: `url(${item.image}) ` }}
     className="shadow-lg shadow-[#040c16] group container rounded-md 
-              flex justify-center text-center items-center mx-auto content-center "
+              flex justify-center text-center items-center mx-auto content-center border-2 "
   >
     <div className="opacity-0 group-hover:opacity-100 ">
       <span className="text-2xl font-bold text-gray-800 tracking-wider bg-gray-50">
