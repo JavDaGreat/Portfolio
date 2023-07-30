3def8dc495733983370584f512d2de24c3a6b5ae
Javad <1998javad@gmail.com>
2023-07-30 16:44:08 +0200

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,9 +20,9 @@ const Navbar = () => {
 
 
   return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300 shadow-lg'>
       <div>
-        <img src={Logo} alt='Logo Image' style={{ width: '150px' }} />
+        <img src={Logo} alt='Logo Image'  className="contains h-[80px] w-36" />
       </div>
 
       {/* menu */}
